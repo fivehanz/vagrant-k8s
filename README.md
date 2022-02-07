9f45cb37a3adf18568398b7c9ae640e69d2811c4
Hanz <haniel56@zoho.eu>
2022-02-07 19:47:18 +0530

    deprecated init

---
+++ README.md
@@ -0,0 +1,4 @@
+# vagrant k8s (deprecated)
+
+- vagrant k8s is to be replaced with [rancher desktop](https://rancherdesktop.io/)
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -13,40 +13,38 @@ sudo yum remove -y docker \
 
 # https://docs.docker.com/engine/install/centos/
 sudo yum install -y yum-utils
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-sudo systemctl start docker
+#sudo yum-config-manager \
+#    --add-repo \
+#    https://download.docker.com/linux/centos/docker-ce.repo
+#sudo yum install -y docker-ce docker-ce-cli containerd.io
+#sudo systemctl enable docker.service
+#sudo systemctl enable containerd.service
+#sudo systemctl start docker
 
 # install kubectl
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
+#cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+#[kubernetes]
+#name=Kubernetes
+#baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+#enabled=1
+#gpgcheck=1
+#repo_gpgcheck=1
+#gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+#EOF
 
-sudo dnf update -y
-sudo yum install -y kubectl
+#sudo dnf update -y
+#sudo yum install -y kubectl
 
 # minikube installation
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
-sudo rpm -Uvh minikube-latest.x86_64.rpm
+#curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
+#sudo rpm -Uvh minikube-latest.x86_64.rpm
 
 # update and install git, htop
-sudo dnf update -y
-sudo dnf install git htop golang -y
+#sudo dnf update -y
+#sudo dnf install git htop go -y
 
-# install helm 
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+
+
 
 
 
